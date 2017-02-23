f435e5fc20186367f8cd32ec91b03f0b4213d088
Brian Cavalier <brian@briancavalier.com>
2017-02-23 07:47:23 -0500

    test(index-test): Add more test encouragement (#239)

--- test/index-test.js
+++ test/index-test.js
@@ -5,10 +5,10 @@ import assert from 'assert'
 
 import awesome from '../src/index'
 
-// TODO: Write more tests
-
 describe('@most/mostPackage', () => {
   it('should be awesome', () => {
     assert(awesome(true))
   })
+
+  it('TODO: add more tests')
 })
